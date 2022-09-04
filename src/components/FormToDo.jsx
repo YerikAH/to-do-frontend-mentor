ad7d8d150a0f4005dafbafb7ef8c423afe370fca
Harvey <yerik05vh@gmail.com>
2022-09-04 16:21:58 -0500

    add logic todo

--- src/components/FormToDo.jsx
+++ src/components/FormToDo.jsx
@@ -8,7 +8,7 @@ import ToDo from "./ToDo";
 const Form = styled.form`
   display: flex;
   justify-content: center;
-  margin: 2rem 0;
+  margin-top: 2rem;
   padding: 0 1.5rem;
 `;
 const InputAdd = styled.input`
@@ -16,31 +16,49 @@ const InputAdd = styled.input`
   height: 20px;
   border-radius: 255px;
   background-color: var(--very-light-gray);
-  border: 1px solid var(--very-dark-grayish-blue);
+  border: 1px solid var(--light-grayish-blue-hover);
   margin-right: 1rem;
 `;
 const InputText = styled.input`
   border: none;
   outline: none;
+  font-size: 0.9rem;
 `;
 const DivForm = styled.div`
   display: flex;
   background-color: var(--very-light-gray);
   width: 100%;
   align-items: center;
-  border-radius: 0.5em;
+  border-radius: 0.4em;
   padding: 1rem;
 `;
-const DivContainer = styled.div``;
+const DivContainer = styled.div`
+  padding: 1rem 1.5rem;
+`;
 const Div = styled.div`
   background-color: var(--very-light-gray);
+  border-radius: 0.4em;
+`;
+const DivCount = styled.div`
+  display: flex;
+  padding: 1rem;
+  justify-content: space-between;
+`;
+const ParCount = styled.p`
+  font-size: 0.9rem;
+  color: var(--dark-grayish-blue);
+`;
+const ButtonClear = styled.button`
+  border: none;
+  background: transparent;
+  font-size: 0.9rem;
+  color: var(--dark-grayish-blue);
 `;
 
 export default function FormToDo() {
   const [work, setWork] = useState("");
   const [form, setForm] = useState([]);
   const handleClick = (e) => {
-    //I don't know if this has any effect
     e.preventDefault();
     let workInfo = {
       id: Date.now(),
@@ -48,11 +66,28 @@ export default function FormToDo() {
       state: false,
     };
     setForm([...form, workInfo]);
+    setWork("");
+  };
+  const handleRemoveTask = (id) => {
+    let varTemp = [...form];
+    let otherVar = varTemp.filter((item) => item.id !== id);
+    setForm(otherVar);
+  };
+  const handleEdit = (id) => {
+    let varTemp = [...form];
+    let otherVar = varTemp.find((item) => item.id === id);
+    otherVar.state = !otherVar.state;
+    setForm(varTemp);
+  };
+  const handleClearCompleted = () => {
+    let varTemp = [...form];
+    let otherVar = varTemp.filter((item) => item.state !== true);
+    setForm(otherVar);
   };
 
   return (
     <>
-      <Form action="">
+      <Form onSubmit={handleClick}>
         <DivForm>
           <InputAdd type="button" value="" onClick={handleClick} />
           <InputText
@@ -63,11 +98,25 @@ export default function FormToDo() {
           />
         </DivForm>
       </Form>
+
       <DivContainer>
         <Div>
           {form.map((item) => (
-            <ToDo key={item.id} work={item.work} state={item.state} />
+            <ToDo
+              key={item.id}
+              work={item.work}
+              state={item.state}
+              ident={item.id}
+              handleRemoveTask={handleRemoveTask}
+              handleEdit={handleEdit}
+            />
           ))}
+          <DivCount>
+            <ParCount>{form.length} items left</ParCount>
+            <ButtonClear onClick={handleClearCompleted}>
+              Clear Completed
+            </ButtonClear>
+          </DivCount>
         </Div>
       </DivContainer>
     </>
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -9,38 +9,76 @@ import checkstatus from "../images/icon-check.svg";
 //styles
 const Div = styled.div`
   display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid var(--light-grayish-blue);
+  overflow: auto;
 `;
 const Button = styled.button`
   width: 20px;
   height: 20px;
   background: ${(props) =>
     props.active ? "var(--check-background)" : "transparent"};
+
+  border-radius: 255px;
+  border: ${(props) =>
+    props.active ? "none" : "1px solid var(--light-grayish-blue-hover)"};
+
+  outline: none;
+  margin-right: 10px;
+  display: grid;
+  place-items: center;
+  place-content: center;
 `;
-const Par = styled.p``;
-const Img = styled.img``;
+const GroupDiv = styled.div`
+  display: flex;
+`;
+const Par = styled.p`
+  color: ${(props) =>
+    props.active
+      ? "var(--light-grayish-blue)"
+      : "var(--very-dark-grayish-blue)"};
+  align-items: center;
+  font-size: 0.9rem;
+  display: flex;
+`;
+const Img = styled.img`
+  width: 14px;
+  height: 14px;
+`;
+const ButtonDelete = styled.button`
+  background: transparent;
+  border: none;
+`;
 const ImgCheck = styled.img``;
-export default function ToDo({ work, state }) {
+export default function ToDo({
+  work,
+  state,
+  ident,
+  handleRemoveTask,
+  handleEdit,
+}) {
   return (
     <>
       <Div>
-        {!state ? (
-          <Button></Button>
-        ) : (
-          <Button active>
-            <ImgCheck src={checkstatus} />
-          </Button>
-        )}
-        {!state ? (
-          <Par>{work}</Par>
-        ) : (
-          <Par active>
-            <strike>
-              <i>{work}</i>
-            </strike>
-          </Par>
-        )}
+        <GroupDiv>
+          {!state ? (
+            <Button onClick={() => handleEdit(ident)}></Button>
+          ) : (
+            <Button active onClick={() => handleEdit(ident)}>
+              <ImgCheck src={checkstatus} />
+            </Button>
+          )}
+          {!state ? (
+            <Par>{work}</Par>
+          ) : (
+            <Par active>
+              <del>{work}</del>
+            </Par>
+          )}
+        </GroupDiv>
 
-        <Img src={deletetask} />
+        <Img src={deletetask} onClick={() => handleRemoveTask(ident)} />
       </Div>
     </>
   );
